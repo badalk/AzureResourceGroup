4ef2c60e2f158d1c070e45c1f43acd5d2ec752a7
Badal Kotecha <badal.kotecha@mindtree.com>
2018-08-17 10:49:35 -0400

    generalized tests and added json data

--- AzureResourceGroup1/rg-AKS/azuredeploy-acr.tests.ps1
+++ AzureResourceGroup1/rg-AKS/azuredeploy-acr.tests.ps1
@@ -1,14 +1,25 @@
 ﻿Param(
-  [string] [Parameter(Mandatory=$true)] $ResourceGroupName,
-  [string] [Parameter(Mandatory=$true)] $TemplateFile,
-  [hashtable] [Parameter(Mandatory=$true)] $PassedParameters
+  [string] [Parameter(Mandatory=$true)] $ResourceGroupName
+  #[string] [Parameter(Mandatory=$true)] $TemplateFile,
+  #[hashtable] [Parameter(Mandatory=$true)] $PassedParameters
 )
 
 Describe "Azure Container Registry Deployment Tests" {
-	#Arrange
-	#$PassedParameters = (get-content "$TemplateParameterFile" | ConvertFrom-Json -ErrorAction SilentlyContinue).parameters
+	# #################
+	# ## Arrange ##
+	# #################
 
-	$TemplateParameters = (get-content "$TemplateFile" | ConvertFrom-Json -ErrorAction SilentlyContinue).parameters
+	#$PassedParameters = (get-content "$TemplateParameterFile" | ConvertFrom-Json -ErrorAction SilentlyContinue).parameters
+	$TemplateFileName = $MyInvocation.MyCommand.Name.Replace("Tests.ps1", "json") #Getting ARM template file name (extension is json)
+	$TemplateFile = "${PSScriptRoot}\${TemplateFileName}"
+	$TemplateParameterDefinitions = (get-content "$TemplateFile" | ConvertFrom-Json -ErrorAction SilentlyContinue).parameters
+
+	#Load Test Data
+	#Load data based on the data file as per the convention
+	$TestsDataFile = $MyInvocation.MyCommand.Name.Replace("ps1", "Data.json") #Getting Tests Data file name (extension is json)
+	$PassedParameters = (Get-Content -Raw -Path $TestsDataFile) | ConvertFrom-Json
+
+	#Determine if we should we skip replication test cases
 	$IsReplicationEnabled = ($PassedParameters.isReplicationEnabled) -and ($PassedParameters.sku -eq 'Premium')
 	$Skiptests = @{ 'Skip' = !$IsReplicationEnabled }
 
@@ -36,7 +47,10 @@ Describe "Azure Container Registry Deployment Tests" {
 		$DebugPreference = "SilentlyContinue"
 	}
 
-	#Assert
+	# #######################
+	# ###### Assert #########
+	# #######################
+
 	#Context "When Azure Container Registry is deployed without parameters" {
 	#	try {
 	#		$output = Test-AzureRmResourceGroupDeployment `
@@ -58,51 +72,56 @@ Describe "Azure Container Registry Deployment Tests" {
 
 	Context "Check for Valid Parameter Values"  {
 
-		it ("Container Registry location parameter passed must be within allowed values"){
-			$PassedParameters.location | should -BeIn $TemplateParameters.location.allowedValues
+		it ("Container Registry location parameter passed must be within allowed values") -TestCases $PassedParameters {
+			Param($param)
+			$param.location | should -BeIn $TemplateParameterDefinitions.location.allowedValues
 		}
 
-		Context "Container Registry Replication Parameters Tests" {
-			it @Skiptests ("Container Registry Replication Location parameter must be different than the provisioning location"){
-				$PassedParameters.replicatedregistrylocation | should -Not -Be $PassedParameters.location.value
-			}
+		it @Skiptests ("Container Registry Replication Location parameter must be different than the provisioning location")-TestCases $PassedParameters {
+			Param($param)
+			$param.replicatedregistrylocation | should -Not -Be $PassedParameters.location.value
+		}
 
-			it @Skiptests ("Container Registry Replication Location parameter must be within allowed values"){
-				$PassedParameters.replicatedregistrylocation | should -BeIn $TemplateParameters.replicatedregistrylocation.allowedValues
-			}
+		it @Skiptests ("Container Registry Replication Location parameter must be within allowed values")-TestCases $PassedParameters {
+			Param($param)
+			$param.replicatedregistrylocation | should -BeIn $TemplateParameterDefinitions.replicatedregistrylocation.allowedValues
 		}
 
 	}
 
 	Context "When Azure Container Registry (ACR) is deployed with valid parameters" { 
 		#$PassedParameters = (get-content "$TemplateParameterFile" | ConvertFrom-Json -ErrorAction SilentlyContinue).parameters
+		function UnitTest-AzureRmDeployment {
+			param ($param)
 
-		$output = Test-AzureRmResourceGroupDeployment `
-				-ResourceGroupName $ResourceGroupName `
-				-TemplateFile $TemplateFile `
-				-TemplateParameterObject $PassedParameters `
-				-ErrorAction Stop `
-					5>&1
+			$output = Test-AzureRmResourceGroupDeployment `
+					-ResourceGroupName $ResourceGroupName `
+					-TemplateFile $TemplateFile `
+					-TemplateParameterObject $param `
+					-ErrorAction Stop `
+						5>&1
 
-		$responseItem = $output -like "*HTTP RESPONSE*"
-		#Write-Output $responseItem
+			$responseItem = $output -like "*HTTP RESPONSE*"
+			$httpresponse = ($responseItem -split "Body:")[1]
+			$result = ($httpresponse | ConvertFrom-Json).properties
 
-		$httpresponse = ($responseItem -split "Body:")[1]
-		#Write-Output $httpresponse
+			It "ACR deployment should succeed" {
+				$result.provisioningState | Should -Be "Succeeded"
+			}
 
-		$result = ($httpresponse | ConvertFrom-Json).properties
-
-		It "ACR deployment should succeed" {
-			$result.provisioningState | Should -Be "Succeeded"
-		}
-
-		It "If service tier is not premium - no registry relication irrespective of isReplicationEnabled is set to true or false" {
+			It "If service tier is not premium - no registry relication irrespective of isReplicationEnabled is set to true or false" {
 			
+			}
+
+			It "If service tier is premium then only allow replicated regions" {
+
+			}
 		}
 
-		It "If service tier is premium then only allow replicated regions" {
-
+		foreach ($param in $PassedParameters){
+			UnitTest-AzureRmDeployment($param)
 		}
+
 	}
 
 
