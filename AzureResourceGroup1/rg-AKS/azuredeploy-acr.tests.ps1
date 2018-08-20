bd9dfce60743524af6abfd758e3286e88a2cb336
Badal Kotecha <badal.kotecha@mindtree.com>
2018-08-20 00:29:03 -0400

    added function locally

--- AzureResourceGroup1/rg-AKS/azuredeploy-acr.tests.ps1
+++ AzureResourceGroup1/rg-AKS/azuredeploy-acr.tests.ps1
@@ -13,7 +13,36 @@ Describe "Azure Container Registry Deployment Tests" {
 	# #################
 	# ## Arrange ##
 	# #################
-	Import-Module "${PSScriptRoot}\ConvertTo-SplattedHashtable.psm1"
+	function ConvertTo-SplattedHashtable {
+		[CmdletBinding()]
+		[OutputType('hashtable')]
+		param (
+			[Parameter(ValueFromPipeline)]
+			$InputObject
+		)
+ 
+		process {
+
+			$testcases = @()
+			foreach($param in $InputObject){
+
+				$theObject = $param.parameters.psObject.Properties 
+
+				Write-Host ("theObject:" + $theObject)
+				$ht2 = @{}
+
+				foreach($object in $theObject)
+				{
+					$ht2[$object.Name] = $object.Value.value
+				}
+
+				Write-Host("ht2 = " + $ht2.psobject.properties)
+				$testcases += $ht2
+		   }
+
+		   $testcases
+		}
+	}
 
 	BeforeAll { #Enable DebugPreference to Continue to capture Debug info
 		$DebugPreference = "Continue"
