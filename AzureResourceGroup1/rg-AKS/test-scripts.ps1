9cbe97612322ec725a6c65999116cea9f370b1f4
Badal Kotecha <badal.kotecha@mindtree.com>
2018-10-19 14:52:14 -0400

    adding K8s ingress controller scripts

---
+++ AzureResourceGroup1/rg-AKS/test-scripts.ps1
@@ -0,0 +1,11 @@
+﻿$ResourceGroupName = "rg-aks-arm-cicd"
+$TemplateFile = "C:\MyWork\Azure\ARM\ARM-FirstSample\AzureResourceGroup1\AzureResourceGroup1\rg-AKS\azuredeploy-aks.json"
+$TemplateParametersFile = "C:\MyWork\Azure\ARM\ARM-FirstSample\AzureResourceGroup1\AzureResourceGroup1\rg-AKS\azuredeploy-aks-cicd.parameters.json"
+
+New-AzureRmResourceGroupDeployment -Name ((Get-ChildItem $TemplateFile).BaseName + '-' + ((Get-Date).ToUniversalTime()).ToString('MMdd-HHmm')) `
+                                   -ResourceGroupName $ResourceGroupName `
+                                   -TemplateFile $TemplateFile `
+                                   -TemplateParameterFile $TemplateParametersFile `
+                                   -Mode Incremental `
+                                   -Force -Verbose `
+                                   -ErrorVariable ErrorMessages
