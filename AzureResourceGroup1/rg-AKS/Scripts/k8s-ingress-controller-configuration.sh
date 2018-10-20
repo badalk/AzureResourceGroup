6b8726f4410a7bd9876b6966d450a21e9ccf4682
Badal Kotecha <badal.kotecha@mindtree.com>
2018-10-20 10:43:18 -0400

    moified script

--- AzureResourceGroup1/rg-AKS/Scripts/k8s-ingress-controller-configuration.sh
+++ AzureResourceGroup1/rg-AKS/Scripts/k8s-ingress-controller-configuration.sh
@@ -5,8 +5,10 @@ nodeResourceGroup="$(az aks show --resource-group ${aksResourceGroup} --name ${a
 
 echo "${nodeResourceGroup}"
 
+echo "creating a public ip for ingress controller"
 az network public-ip create --resource-group "${nodeResourceGroup}" --name aksIngressPublicIP --allocation-method static
 
-ingressPublicIp="$(az network public-ip show --resource-group ${nodeResourceGroup} --name aksIngressPublicIP)"
+echo "getting newly created public ip for ingress controller"
+ingressPublicIp="$(az network public-ip show -g ${nodeResourceGroup} -n aksIngressPublicIP --query \"{address: ipAddress}\")"
  
 echo "ingress public ip ${ingressPublicIp}"
