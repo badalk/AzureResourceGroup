bae4db3f2033678fa23876837592c84fdc6a13a0
Badal Kotecha <badal.kotecha@mindtree.com>
2018-10-22 14:30:28 -0400

    added error handling

--- AzureResourceGroup1/rg-AKS/Scripts/k8s-ingress-controller-configuration.sh
+++ AzureResourceGroup1/rg-AKS/Scripts/k8s-ingress-controller-configuration.sh
@@ -1,17 +1,28 @@
 aksResourceGroup=$1
 aksName=$2
 
+PROGNAME=$(basename $0)
+
+error_exit()
+{
+	echo "${PROGNAME}: ${1:-"Unknown Error"}" 1>&2
+	exit 1
+}
+
 nodeResourceGroup="$(az aks show --resource-group ${aksResourceGroup} --name ${aksName} --query nodeResourceGroup -o tsv)"
 
 echo "${nodeResourceGroup}"
-
 echo "creating a public ip for ingress controller"
 az network public-ip create --resource-group "${nodeResourceGroup}" --name aksIngressPublicIP --allocation-method static
 
-echo "getting newly created public ip for ingress controller"
-ingressPublicIp="$(az network public-ip show -g ${nodeResourceGroup} -n aksIngressPublicIP --query ipAddress -o tsv)"
+if [ "$?" = "0" ]; then
+	echo "getting newly created public ip for ingress controller"
+	ingressPublicIp="$(az network public-ip show -g ${nodeResourceGroup} -n aksIngressPublicIP --query ipAddress -o tsv)"
 
-echo "Ingress public ip ${ingressPublicIp}"
+	echo "Ingress public ip ${ingressPublicIp}"
+else
+	error_exit "$LINENO: Cannot create public ip in node resource group ${nodeResourceGroup} for aks given resource group ${aksResourceGroup} and cluster name ${aksName}" 1>&2
+fi
 
 echo "Install helm client ......."
 sudo snap install helm --classic
@@ -24,7 +35,7 @@ kubectl create -f "$MY_PATH/../Resources/helm-rbac.yaml"
 
 echo "Copying Kube context into helm ......."
 mkdir ~/snap/helm/common/kube
-cp -i ~/.kube/config ~/snap/helm/common/kube/
+cp -f ~/.kube/config ~/snap/helm/common/kube/
 
 echo "Install Tiller service and initialize helm ......."
 helm init --service-account tiller
