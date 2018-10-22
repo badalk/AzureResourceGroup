ed692de1830468dc961e84811e542922e5a449b2
Badal Kotecha <badal.kotecha@mindtree.com>
2018-10-21 21:30:32 -0400

    added rest of the steps to install ingress controller

--- AzureResourceGroup1/rg-AKS/Scripts/k8s-ingress-controller-configuration.sh
+++ AzureResourceGroup1/rg-AKS/Scripts/k8s-ingress-controller-configuration.sh
@@ -9,6 +9,21 @@ echo "creating a public ip for ingress controller"
 az network public-ip create --resource-group "${nodeResourceGroup}" --name aksIngressPublicIP --allocation-method static
 
 echo "getting newly created public ip for ingress controller"
-ingressPublicIp="$(az network public-ip show -g ${nodeResourceGroup} -n aksIngressPublicIP --query \"{address: ipAddress}\")"
- 
-echo "ingress public ip ${ingressPublicIp}"
+ingressPublicIp="$(az network public-ip show -g ${nodeResourceGroup} -n aksIngressPublicIP --query ipAddress -o tsv)"
+
+echo "Ingress public ip ${ingressPublicIp}"
+
+echo "Install helm client ......."
+snap install helm --classic
+
+echo "Creating tiller service account and rolebinding ......."
+kubectl create -f /../Resources/helm-rbac.yaml
+
+echo "Copying Kube context into helm ......."
+cp -i ~/.kube/config ~/snap/helm/common/kube/
+
+echo "Install Tiller service and initialize helm ......."
+helm init --service-account tiller
+
+echo "Install ingress service with poblic Ip ${ingressPublicIp}"
+helm install stable/nginx-ingress --namespace kube-system --set controller.service.loadBalancerIP="${ingressPublicIp}"
