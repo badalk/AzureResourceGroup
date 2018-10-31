36942c8258965780c9d638806af8126ebe3eb352
Badal Kotecha <badal.kotecha@mindtree.com>
2018-10-30 21:31:02 -0400

    corrected the path for copying .config

--- AzureResourceGroup1/rg-AKS/Scripts/k8s-install-helm.sh
+++ AzureResourceGroup1/rg-AKS/Scripts/k8s-install-helm.sh
@@ -15,13 +15,13 @@ kubectl create -f "$MY_PATH/../Resources/helm-rbac.yaml"
 
 echo "Create helm kube config folder if one does not exist"
 if [ -d "/path/to/dir" ]; then
-	mkdir /snap/helm/common/kube
+	mkdir ~/snap/helm/common/kube
 else
-	rm /snap/helm/common/kube/config	#remove config file if one exists already as it may be from previous deployment
+	rm ~/snap/helm/common/kube/config	#remove config file if one exists already as it may be from previous deployment
 fi
 
 echo "Copying Kube context into helm ......."
-cp -f ~/.kube/config ~/snap/helm/common/kube/ 
+cp -f /.kube/config ~/snap/helm/common/kube/ 
 
 echo "Install Tiller service and initialize helm ......."
 helm init --service-account tiller
