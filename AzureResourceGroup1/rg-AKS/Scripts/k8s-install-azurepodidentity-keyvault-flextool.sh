4b813b6af6ac6c66f648bea78757ba38b67fce56
Badal Kotecha <badal.kotecha@mindtree.com>
2018-11-09 10:55:47 -0500

    commented code which was creating an MSI, as now we are assuming that MSI will be pre-created and it will have permissions to read secrets, certificates and keys from Keyvault

--- AzureResourceGroup1/rg-AKS/Scripts/k8s-install-azurepodidentity-keyvault-flextool.sh
+++ AzureResourceGroup1/rg-AKS/Scripts/k8s-install-azurepodidentity-keyvault-flextool.sh
@@ -4,8 +4,7 @@ aksResourceGroup=$1
 aksName=$2
 acrname=$3
 keyVaultResourceId=$4
-clientID=$5
-azurePodIdentityName=$6
+azurePodIdentityResourceId=$5
 
 PROGNAME=$(basename $0)
 
@@ -32,35 +31,49 @@ echo "Creating aadpodidentity resources ......."
 kubectl create -f "$MY_PATH/../Resources/deployment-rbac.yaml" 
 
 
-echo "AKS Node Resource Group (\$nodeResourceGroup): '${nodeResourceGroup}'"
-# create managed service identity and get the client id
-## TODO: we need to check if we will be pre-creating the msi and just using its client id and principalid or for each cluster we need to create a new msi
+##############################################################################################################################
+# All of this below can be pre-created (MSI, Granting MSI permission to the key vault to read secrets, certificates and keys)
+# If not, uncomment and service principal should have permission to read and write directory data, which may be difficult
+##############################################################################################################################
+
+# echo "AKS Node Resource Group (\$nodeResourceGroup): '${nodeResourceGroup}'"
+# # create managed service identity and get the client id
+# ## TODO: we need to check if we will be pre-creating the msi and just using its client id and principalid or for each cluster we need to create a new msi
+# echo "Creating an msi to access key vault...."
+# clientId="$(az identity create -g ${nodeResourceGroup} -n ${azurePodIdentityName} --query clientId -o tsv)"
+# echo "MSI Client Id: ${clientId}"
+# Id="$(az identity show -g ${nodeResourceGroup} -n ${azurePodIdentityName} --query id -o tsv)"
+# echo "MSI Resorce Id: ${Id}"
+# principalId="$(az identity show -g ${nodeResourceGroup} -n ${azurePodIdentityName} --query principalId -o tsv)"
+# echo "MSI Principal Id: ${principalId}"
+
+# echo "MSI Client ID: ${clientId}, Id: ${Id}, principalId: ${principalId}"
+
+# # Assign Reader Role to new Identity for your keyvault
+# az role assignment create --role Reader --assignee ${principalId} --scope ${keyVaultResourceId}
+
+# KV_NAME="aks-sp-key-secret-vault" #TODO: get it from keyvault resoure ID parameter
+# # set policy to access keys in your keyvault
+# az keyvault set-policy -n ${KV_NAME} --key-permissions get --spn ${clientId}
+# # set policy to access secrets in your keyvault
+# az keyvault set-policy -n ${KV_NAME} --secret-permissions get --spn ${clientId}
+# # set policy to access certs in your keyvault
+# az keyvault set-policy -n ${KV_NAME} --certificate-permissions get --spn ${clientId}
+
+######################################################################################
+# 
+######################################################################################
 echo "Creating an msi to access key vault...."
-clientId="$(az identity create -g ${nodeResourceGroup} -n k8s-msi --query clientId -o tsv)"
+clientId="$(az identity show --ids ${azurePodIdentityResourceId} --query clientId -o tsv)"
 echo "MSI Client Id: ${clientId}"
-Id="$(az identity show -g ${nodeResourceGroup} -n k8s-msi --query id -o tsv)"
-echo "MSI Resorce Id: ${Id}"
-principalId="$(az identity show -g ${nodeResourceGroup} -n k8s-msi --query principalId -o tsv)"
+principalId="$(az identity show ${azurePodIdentityResourceId} --query principalId -o tsv)"
 echo "MSI Principal Id: ${principalId}"
-
-echo "MSI Client ID: ${clientId}, Id: ${Id}, principalId: ${principalId}"
-
-# Assign Reader Role to new Identity for your keyvault
-az role assignment create --role Reader --assignee ${principalId} --scope ${keyVaultResourceId}
-
-KV_NAME="aks-sp-key-secret-vault" #TODO: get it from keyvault resoure ID parameter
-# set policy to access keys in your keyvault
-az keyvault set-policy -n ${KV_NAME} --key-permissions get --spn ${clientId}
-# set policy to access secrets in your keyvault
-az keyvault set-policy -n ${KV_NAME} --secret-permissions get --spn ${clientId}
-# set policy to access certs in your keyvault
-az keyvault set-policy -n ${KV_NAME} --certificate-permissions get --spn ${clientId}
 
 #package and add aadpodidentity helm chart to ACR repository, TODO: if does not exist
 az configure --defaults acr=${acrname}
 az acr helm repo add
 #cd "$MY_PATH/../Resources/"
-helm package "$MY_PATH/../Resources/aadpodidentity"
+helm package "$MY_PATH/../Resources/aadpodidentity" --destination "$MY_PATH/../Resources""
 az acr helm push "$MY_PATH/../Resources/adpodidentity-1.0.0.tgz"
 
 helm install aadpodidentity --set name=${azurePodIdentityName} --set msi.resourceID=${keyVaultResourceId} --set msi.clientID=${clientId}
