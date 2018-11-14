255011d57dd0782d056a92db86bb5444f84041cc
Badal Kotecha <badal.kotecha@mindtree.com>
2018-11-14 12:22:59 -0500

    changed chart name in the script for cluster admin

--- AzureResourceGroup1/rg-AKS/Scripts/k8s-install-cluster-admin-rolebinding.sh
+++ AzureResourceGroup1/rg-AKS/Scripts/k8s-install-cluster-admin-rolebinding.sh
@@ -23,10 +23,10 @@ echo "$MY_PATH"
 echo "Creating cluster admib rolebinding"
 
 echo "Packaging cluster admin role helm chart ...."
-helm package "$MY_PATH/../Resources/k8s-cluster-admin" --destination "$MY_PATH/../Resources"
+helm package "$MY_PATH/../Resources/k8sClusterAdmin" --destination "$MY_PATH/../Resources"
 echo "Pushing helm chart to ACR ..."
 #az login --service-principal -u "a03158e9-58f5-4e5e-b11b-b5a2df77c661" -p "FiS+yQNCI7GrX4jJ5ydyZOH9XmbCFCSBN70SdiwwtWg=" --tenant "b25fcb44-9c49-413c-9fdc-b59b39447b84"
-az acr helm push -n "${acrName}" "$MY_PATH/../Resources/k8s-cluster-admin-1.0.0.tgz" --force #--username a03158e9-58f5-4e5e-b11b-b5a2df77c661 --password FiS+yQNCI7GrX4jJ5ydyZOH9XmbCFCSBN70SdiwwtWg=
+az acr helm push -n "${acrName}" "$MY_PATH/../Resources/k8sClusterAdmin-1.0.0.tgz" --force #--username a03158e9-58f5-4e5e-b11b-b5a2df77c661 --password FiS+yQNCI7GrX4jJ5ydyZOH9XmbCFCSBN70SdiwwtWg=
 
 ##########################################################################################
 # Above can be removed if MSI is pre-created
@@ -35,6 +35,6 @@ az acr helm push -n "${acrName}" "$MY_PATH/../Resources/k8s-cluster-admin-1.0.0.
 # Assuming helm chart already exists (if not use the same as above commands to package and push to ACR repository)
 helm repo update
 echo "Installing helm chart for cluster admins on aks cluster ..."
-helm install "${acrName}/k8s-cluster-admin" --set adminAADGoupId="${k8sAdminGroupId}"
+helm install "${acrName}/k8sClusterAdmin" --set adminAADGoupId="${k8sAdminGroupId}"
 
 #kubectl create -f "$MY_PATH/../Resources/cluster-admins.yaml"
