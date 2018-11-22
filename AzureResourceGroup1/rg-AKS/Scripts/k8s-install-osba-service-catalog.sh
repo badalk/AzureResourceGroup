3bf9231aef655cd92d6a49886804a178112c65c1
Badal Kotecha <badal.kotecha@mindtree.com>
2018-11-22 16:33:32 -0500

    added sleep for 5 seconds as installing open service broker is not getting installed due to server not accepting request

--- AzureResourceGroup1/rg-AKS/Scripts/k8s-install-osba-service-catalog.sh
+++ AzureResourceGroup1/rg-AKS/Scripts/k8s-install-osba-service-catalog.sh
@@ -30,5 +30,7 @@ helm repo update
 
 #helm inspect azure/open-service-broker-azure --debug
 
+sleep 5
+
 echo "Install the Open Service Broker for Azure using the Helm chart"
 helm install azure/open-service-broker-azure --name osba --namespace osba --set azure.subscriptionId=${subscriptionId} --set azure.tenantId=${tenantId} --set azure.clientId=${clientId} --set azure.clientSecret=${clientSecret} --wait --debug
