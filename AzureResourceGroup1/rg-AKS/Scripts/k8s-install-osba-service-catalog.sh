24942e8f87a6047680c0e869026d80a01fc37289
Badal Kotecha <badal.kotecha@mindtree.com>
2018-11-24 22:25:56 -0500

    setting rbacEnable to false

--- AzureResourceGroup1/rg-AKS/Scripts/k8s-install-osba-service-catalog.sh
+++ AzureResourceGroup1/rg-AKS/Scripts/k8s-install-osba-service-catalog.sh
@@ -20,7 +20,7 @@ echo "Adding google service catalog repo"
 helm repo add svc-cat https://svc-catalog-charts.storage.googleapis.com
 
 echo "Install Service Catalog"
-helm install svc-cat/catalog --name catalog --namespace catalog --set controllerManager.healthcheck.enabled=false --wait
+helm install svc-cat/catalog --name catalog --namespace catalog --set controllerManager.healthcheck.enabled=false --set rbacEnable=false --wait
 
 echo "Getting status of apiservice"
 kubectl get apiservice
@@ -36,4 +36,4 @@ helm repo add azure https://kubernetescharts.blob.core.windows.net/azure
 sleep 5
 
 echo "Install the Open Service Broker for Azure using the Helm chart"
-helm install azure/open-service-broker-azure --name osba --namespace osba --set azure.subscriptionId=${subscriptionId} --set azure.tenantId=${tenantId} --set azure.clientId=${clientId} --set azure.clientSecret=${clientSecret} --wait --debug
+helm install azure/open-service-broker-azure --name osba --namespace osba --set azure.subscriptionId=${subscriptionId} --set azure.tenantId=${tenantId} --set azure.clientId=${clientId} --set azure.clientSecret=${clientSecret} --set image.pullPolicy=Always --wait --debug
