565bc232b30ca521925b06cf19ae1838df90a48d
Badal Kotecha <badal.kotecha@mindtree.com>
2018-11-24 23:13:48 -0500

    trying few options in svc-cat/catalog installations as the pods are not up and crashing continuously

--- AzureResourceGroup1/rg-AKS/Scripts/k8s-install-osba-service-catalog.sh
+++ AzureResourceGroup1/rg-AKS/Scripts/k8s-install-osba-service-catalog.sh
@@ -20,7 +20,7 @@ echo "Adding google service catalog repo"
 helm repo add svc-cat https://svc-catalog-charts.storage.googleapis.com
 
 echo "Install Service Catalog"
-helm install svc-cat/catalog --name catalog --namespace catalog --set controllerManager.healthcheck.enabled=false --set rbacEnable=false --wait
+helm install svc-cat/catalog --name catalog --namespace catalog --wait --debug
 
 echo "Getting status of apiservice"
 kubectl get apiservice
@@ -38,13 +38,3 @@ sleep 120
 echo "Install the Open Service Broker for Azure using the Helm chart"
 helm install azure/open-service-broker-azure --name osba --namespace osba --set azure.subscriptionId=${subscriptionId} --set azure.tenantId=${tenantId} --set azure.clientId=${clientId} --set azure.clientSecret=${clientSecret} --set image.pullPolicy=Always --wait --debug
 
-if [ "$?" = "0" ]; then
-
-else
-	echo "got error while installing osba chart.. trying again. Deleting current failed release"
-	helm delete --purge osba
-	echo "Waiting for 2 mins.. before retrying"
-	sleep 120
-	echo "installing again..."
-	helm install azure/open-service-broker-azure --name osba --namespace osba --set azure.subscriptionId=${subscriptionId} --set azure.tenantId=${tenantId} --set azure.clientId=${clientId} --set azure.clientSecret=${clientSecret} --set image.pullPolicy=Always --wait --debug
-fi
