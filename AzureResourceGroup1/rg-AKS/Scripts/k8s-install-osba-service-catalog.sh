f7f1dfdc778397a95ad5af51d7a1601df7f2b35c
Badal Kotecha <badal.kotecha@mindtree.com>
2018-12-02 23:15:12 -0500

    adding changed command and time for service catalog to install properly

--- AzureResourceGroup1/rg-AKS/Scripts/k8s-install-osba-service-catalog.sh
+++ AzureResourceGroup1/rg-AKS/Scripts/k8s-install-osba-service-catalog.sh
@@ -20,7 +20,8 @@ echo "Adding google service catalog repo"
 helm repo add svc-cat https://svc-catalog-charts.storage.googleapis.com
 
 echo "Install Service Catalog"
-helm install svc-cat/catalog --name catalog --namespace catalog --wait --debug
+helm install svc-cat/catalog --name catalog --namespace catalog --wait
+helm install svc-cat/catalog --name catalog --namespace catalog --set apiserver.storage.etcd.persistence.enabled=true --set apiserver.healthcheck.enabled=false --set controllerManager.healthcheck.enabled=false --set apiserver.verbosity=2 --set controllerManager.verbosity=2 --wait
 
 echo "Getting status of apiservice"
 kubectl get apiservice
@@ -28,12 +29,8 @@ kubectl get apiservice
 echo "Add Open Service Broker for Azure Helm repository"
 helm repo add azure https://kubernetescharts.blob.core.windows.net/azure
 
-#echo "Updating the repo"
-#helm repo update
-
-#helm inspect azure/open-service-broker-azure --debug
-
-sleep 90 #wait for couple of mins for serivce controller manager pods to be in ready state. Usually it takes about 50 seconds
+echo "Waiting for few mins to give some time for service catalog control manager and api server to be in running state...."
+sleep 240 #wait for minute for serivce controller manager pods to be in ready state.
 
 echo "Install the Open Service Broker for Azure using the Helm chart"
 helm install azure/open-service-broker-azure --name osba --namespace osba --set azure.subscriptionId=${subscriptionId} --set azure.tenantId=${tenantId} --set azure.clientId=${clientId} --set azure.clientSecret=${clientSecret} --set image.pullPolicy=Always --wait --debug
